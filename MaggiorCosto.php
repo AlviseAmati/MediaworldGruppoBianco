4c0af193860349b0af018a91b09603b85295569f
CejkaFabio <36411541+CejkaFabio@users.noreply.github.com>
2018-09-26 11:37:16 +0200

    4

---
+++ MaggiorCosto.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+</head>
+<body>
+<nav class="navbar navbar-inverse" id="navigation">
+    <form class="form-inline" action ="Login.php">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand">Menù</a>
+            </div>
+            <div class="container">
+            <ul class="nav navbar-form navbar-right">
+            </ul>
+            </div>
+        </div>
+    </form>
+    </nav>
+</body>
+</html>
