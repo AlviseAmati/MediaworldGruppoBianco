afa5da598d990897638dad09591671fcf3c46327
CejkaFabio <36411541+CejkaFabio@users.noreply.github.com>
2018-09-26 17:19:09 +0200

    8

--- Config.php
+++ Config.php
@@ -4,5 +4,3 @@ $username="root";
 $password="";
 $dbname="MediaworldGruppoBianco";
 $perpage=10;
-
-//$db = new PDO ("mysql:host=$hostname;dbname=$dbname", $username, $password, array(PDO::ATTR_PERSISTENT => true));
