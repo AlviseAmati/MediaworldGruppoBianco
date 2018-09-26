3049fbc3a587e48871c8cb1b49c6a4938b8a40a6
FedericoPerra <36411494+FedericoPerra@users.noreply.github.com>
2018-09-26 10:09:29 +0200

    Config.php per collegamento a database

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+$hostname="192.168.245.15";
+$username="root";
+$password="";
+$dbname="MediaworldGruppoBianco";
+$perpage=10;
+
+//$db = new PDO ("mysql:host=$hostname;dbname=$dbname", $username, $password, array(PDO::ATTR_PERSISTENT => true));
