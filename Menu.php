2bd9f9b07a58b741972dde42faa7fdc02139479f
CejkaFabio <36411541+CejkaFabio@users.noreply.github.com>
2018-09-26 10:59:10 +0200

    menu1

---
+++ Menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="scripts/Sorter.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="scripts/Research.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="scripts/AJAX.js"></script> 
+</head>   
+
+<br> <br> <br> <br> <br> <br> <br> 
+<h2 align='center'>
+<a href="GiacenzeMilano.php"> Giacenze Milano </a>
+<br> <br>
+<a href="CarichiRimini.php">Carichi Rimini</a>
+<br> <br>
+<a href="MaggiorCosto.php">Maggior Costo</a>
+</html>
