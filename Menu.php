ca9b47256d059fac0567f3e5eb6a2f2120d6a961
AlviseAmati <36411525+AlviseAmati@users.noreply.github.com>
2018-09-26 11:06:27 +0200

    Menu Alvise

--- Menu.php
+++ Menu.php
@@ -1,22 +1,33 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script language="JavaScript" type="text/JavaScript" src="scripts/Sorter.js"></script>
-    <script language="JavaScript" type="text/JavaScript" src="scripts/Research.js"></script>
-    <script language="JavaScript" type="text/JavaScript" src="scripts/AJAX.js"></script> 
-</head>   
-
-<br> <br> <br> <br> <br> <br> <br> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+</head>
+<body>
+<nav class="navbar navbar-inverse" id="navigation">
+    <form class="form-inline" action ="Login.php">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand">Menu</a>
+            </div>
+            <div class="container">
+            <ul class="nav navbar-form navbar-right">
+            </ul>
+            </div>
+        </div>
+    </form>
+    </nav>
+</body>
 <h2 align='center'>
-<a href="GiacenzeMilano.php"> Giacenze Milano </a>
+<a href="GiacenzeMilano.php"> GiacenzeMilano </a>
 <br> <br>
-<a href="CarichiRimini.php">Carichi Rimini</a>
+<a href="CarichiRimini.php">CarichiRimini</a>
 <br> <br>
-<a href="MaggiorCosto.php">Maggior Costo</a>
+<a href="MaggiorCosto.php">MaggiorCosto</a>
+<br> <br>
+</h2>
+<p id="prova" class="hidden"></p>
 </html>
