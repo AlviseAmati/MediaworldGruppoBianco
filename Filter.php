570c94020b2c2809a42fa7b65010aa7f103b4679
FedericoPerra <36411494+FedericoPerra@users.noreply.github.com>
2018-09-28 15:57:03 +0200

    Paginazioni Filter

--- Filter.php
+++ Filter.php
@@ -49,37 +49,80 @@ try {
     }
     echo"</table>";
 
-    echo'<nav><ul class="pagination">';
+    echo'<nav align="right"><ul class="pagination">';
 
-    if($i-5>0)    {
+    if($page-$pagination>0)    {
         echo'<li class="page-item">
-        <a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.($i-5).'">
+        <a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.($page-$pagination).'">
         <span aria-hidden="true">&laquo;</span>
         <span class="sr-only">Previous</span>
         </a>
         </li>';
     }
-
-    if($page>1)
-    {
-        echo '<li class="page-item"><a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page=1">1</a></li>';
-        for($i=1; $i<=$tot_pagine; $i++)    {
-            echo'<li class="page-item"><a href="View.php?tabella='.$_GET['tabella'].'&page='.$i.'">'.$i.'</a></li>';
-        }
-        if($page<$tot_pagine)
-        echo '<li class="page-item"><a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.$tot_pagine.'">'.$tot_pagine.'</a></li>';
-        if($i+5<=$tot_pagine)    {
-            echo'<li class="page-item">
-            <a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.($i+5).'">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-            </a>
-            </li>';
-        }
-
-        echo '</ul></nav>';
+    else{
+        echo'<li class="page-item">
+        <a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page=1">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+        </a>
+        </li>';
     }
-} 
+    echo '<li class="page-item"><a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page=1">1</a></li>';
+    
+    if($tot_pagine>1)
+    {
+        if($page>$tot_pagine-floor($pagination/2))
+        {
+            if($page-$pagination>0)
+            {
+                for($i=$page-$pagination; $i<=$tot_pagine; $i++)    {
+                    echo'<li class="page-item"><a href="View.php?tabella='.$_GET['tabella'].'&page='.$i.'">'.$i.'</a></li>';
+                }
+            }
+            else
+            {
+                for($i=2; $i<=$pagination; $i++)    {
+                    echo'<li class="page-item"><a href="View.php?tabella='.$_GET['tabella'].'&page='.$i.'">'.$i.'</a></li>';
+                }
+            }
+        }
+        else
+        {
+            if($page>floor($pagination/2)&&$page<$tot_pagine-floor($pagination/2))    {
+                for($i=$page-floor($pagination/2); $i<=$page+floor($pagination/2); $i++)    {
+                    echo'<li class="page-item"><a href="View.php?tabella='.$_GET['tabella'].'&page='.$i.'">'.$i.'</a></li>';
+                }
+            }
+            else
+            {
+                for($i=2; $i<=$pagination; $i++)    {
+                    echo'<li class="page-item"><a href="View.php?tabella='.$_GET['tabella'].'&page='.$i.'">'.$i.'</a></li>';
+                }
+            }
+
+            echo '<li class="page-item"><a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.$tot_pagine.'">'.$tot_pagine.'</a></li>';
+        }
+    }
+    if($page+$pagination<=$tot_pagine)    {
+        echo'<li class="page-item">
+        <a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.($page+$pagination).'">
+        <span aria-hidden="true">&raquo;</span>
+        <span class="sr-only">Next</span>
+        </a>
+        </li>';
+    }
+    else
+    {
+        echo'<li class="page-item">
+        <a class="page-link" href="View.php?tabella='.$_GET['tabella'].'&page='.$tot_pagine.'">
+        <span aria-hidden="true">&raquo;</span>
+        <span class="sr-only">Next</span>
+        </a>
+        </li>';
+    }
+    echo '</ul></nav>';
+    
+}
 catch (PDOException $e) {
     echo $e->getMessage();  
 }
